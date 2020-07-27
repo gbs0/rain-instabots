a6ab5004a05d93d25c28d5029565927cf7425a85
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2020-07-27 10:35:55 -0300

    testing purppeteer

--- rain.js
+++ rain.js
@@ -2,22 +2,27 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
+  // const browser = await puppeteer.launch({args: ['--no-sandbox']});
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('https://www.instagram.com/p/CC_cEcbF3Y0/');
+  await page.goto("https://www.instagram.com/p/CC_cEcbF3Y0/");
   
+  await Promise.all([
+    page.waitForNavigation(),
+    page.click(".Ypffh")
+  ]);
   //const formDiv = document.querySelector(".X7cDz");
   // const textArea = document.querySelector(".Ypffh");
 
   // Get page elements and type
-  const textArea = await page.$(".Ypffh")
-  await textArea.click({clickCount:3})
-  await textArea.type('gb_s0')
+  // const textArea = await page.$(".Ypffh")
+  // await textArea.click({clickCount:3})
+  // await textArea.type('gb_s0')
 
-  await page.click(".sqdOP yWX7d    y3zKF     ")
+  // await page.click(".sqdOP yWX7d    y3zKF     ")
   await page.screenshot({path: 'screenshots/test.png'});
 
-  console.log('Form submitted');
+  console.log('Testing');
 
   await browser.close();
 
